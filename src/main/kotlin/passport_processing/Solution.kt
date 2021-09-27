be0805fcf691f3bc7b4d2903177fb3094884a748
magdamiu <badita.magda@gmail.com>
2021-09-27 14:29:41 +0300

    improved solution for day 4 challenge

--- src/main/kotlin/passport_processing/Solution.kt
+++ src/main/kotlin/passport_processing/Solution.kt
@@ -4,59 +4,68 @@ import java.io.File
 
 // the requirements are here https://adventofcode.com/2020/day/4
 
-val passportKeys = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
-val eyeColors = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+private val passportKeys = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+private val eyeColors = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
 
-const val EMPTY_LINE = "\r\n\r"
+const val BLANK_LINE_WINDOWS = "\n\n"
+const val BLANK_LINE_LINUX_MAC = "\r\n\r\n"
+
 const val NEW_LINE = "\r\n"
 const val SPACE_LINE = "\n"
 const val EMPTY_SPACE = " "
 const val KEY_SEPARATOR = ":"
+
 const val FILE_NAME = "src/main/kotlin/passport_processing/input.txt"
 
 fun main() {
-    processPassports()
+    val passports = readContentFromFile()
+    firstPart(passports)
+    secondPart(passports)
 }
 
-private fun processPassports() {
-    val fileFullText = readFileDirectlyAsText()
-    var countFirstPartValid = 0
-    var countSecondPartValid = 0
+class Passport(private val content: Map<String, String>) {
+    companion object {
+        fun fromString(line: String): Passport {
+            val processedLine = line.replace(SPACE_LINE, EMPTY_SPACE)
+            val fieldsAndValues = processedLine.split(NEW_LINE, SPACE_LINE, EMPTY_SPACE)
 
-    fileFullText.split(EMPTY_LINE).forEach { content ->
-        val inputCurrentPassport = cleanExtraLines(content)
-        val currentPassport = mutableMapOf<String, String>()
-        inputCurrentPassport.split(EMPTY_SPACE).forEach { p ->
-            currentPassport[p.substringBefore(KEY_SEPARATOR)] = p.substringAfter(KEY_SEPARATOR)
-        }
-
-        if (currentPassport.keys.containsAll(passportKeys)) {
-            countFirstPartValid++
-            if (currentPassport.entries.all { it.isValid() }) {
-                countSecondPartValid++
+            val currentPassportMap = mutableMapOf<String, String>()
+            fieldsAndValues.forEach { passport ->
+                currentPassportMap[passport.substringBefore(KEY_SEPARATOR)] = passport.substringAfter(KEY_SEPARATOR)
             }
+            return Passport(currentPassportMap)
         }
     }
-    println(countFirstPartValid)
-    println(countSecondPartValid)
+
+    fun hasAllRequiredFields() = content.keys.containsAll(passportKeys)
+
+    fun hasValidValues() = content.all { (key, value) ->
+        when (key) {
+            "byr" -> value.length == 4 && value.toInt() in 1920..2002
+            "iyr" -> value.length == 4 && value.toInt() in 2010..2020
+            "eyr" -> value.length == 4 && value.toInt() in 2020..2030
+            "hgt" -> when {
+                value.endsWith("cm") -> value.dropLast(2).toInt() in 150..193
+                value.endsWith("in") -> value.dropLast(2).toInt() in 59..76
+                else -> false
+            }
+            "hcl" -> value.matches(Regex("#[0-9a-f]{6}"))
+            "ecl" -> value in eyeColors
+            "pid" -> value.matches(Regex("[0-9]{9}")) // value.length == 9 && value.all(Char::isDigit)
+            else -> true
+        }
+    }
 }
 
-private fun readFileDirectlyAsText(): String = File(FILE_NAME).readText(Charsets.UTF_8)
+private fun readContentFromFile() = File(FILE_NAME)
+    .readText()
+    .split(BLANK_LINE_WINDOWS, BLANK_LINE_LINUX_MAC)
+    .map { Passport.fromString(it) }
 
-private fun cleanExtraLines(content: String) =
-    content.replace(NEW_LINE, EMPTY_SPACE).replace(SPACE_LINE, EMPTY_SPACE)
+private fun firstPart(passports: List<Passport>) {
+    println(passports.count(Passport::hasAllRequiredFields))
+}
 
-private fun Map.Entry<String, String>.isValid(): Boolean = when (key) {
-    "byr" -> value.length == 4 && value.toInt() in 1920..2002
-    "iyr" -> value.length == 4 && value.toInt() in 2010..2020
-    "eyr" -> value.length == 4 && value.toInt() in 2020..2030
-    "hgt" -> when {
-        value.endsWith("cm") -> value.dropLast(2).toInt() in 150..193
-        value.endsWith("in") -> value.dropLast(2).toInt() in 59..76
-        else -> false
-    }
-    "hcl" -> value.matches(Regex("#[0-9a-f]{6}"))
-    "ecl" -> value in eyeColors
-    "pid" -> value.matches(Regex("[0-9]{9}"))
-    else -> true
+private fun secondPart(passports: List<Passport>) {
+    println(passports.count { it.hasValidValues() && it.hasAllRequiredFields() })
 }
